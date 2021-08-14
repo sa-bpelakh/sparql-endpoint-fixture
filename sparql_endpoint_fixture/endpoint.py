ad20727becd0f3e05cca8be877621eb52fb19d5c
Boris Pelakh <boris.pelakh@semanticarts.com>
2021-08-14 13:21:19 -0400

    Added unit tests for requests path.

--- sparql_endpoint_fixture/endpoint.py
+++ sparql_endpoint_fixture/endpoint.py
@@ -1,5 +1,5 @@
 import os
-from urllib.parse import parse_qs
+from urllib.parse import urlparse, parse_qs
 
 import pytest
 # import requests_mock
@@ -10,7 +10,6 @@ from rdflib.plugins.sparql.parser import ParseException
 from rdflib.plugins.sparql.parser import parseUpdate
 from rdflib.util import guess_format
 from requests import Request
-from urllib3.response import HTTPResponse
 
 
 class Endpoint:
@@ -38,43 +37,74 @@ class Endpoint:
             else:
                 self.load_rdf(arg)
 
-        m.post(url=uri, raw=self.handle_post)
-        m.get(url=uri, raw=self.handle_get)
+        m.post(url=uri, text=self.handle_post)
+        m.get(url=uri, text=self.handle_get)
 
         # TODO handle GET/PUT/DELETE/POST/HEAD/PATCH for Graph Protocol
         # https://www.w3.org/TR/2013/REC-sparql11-http-rdf-update-20130321/
 
-    def handle_post(self, request: Request, context: dict) -> HTTPResponse:
-        if request.headers['content-type'] == 'application/x-www-form-urlencoded':
-            parsed = parse_qs(request.data)
-            if 'query' in parsed:
-                return self.process_query(parsed['query'][0], results_format=request.headers.get('accept'),
-                                          graph_uris=parsed.get('using-graph-uri'),
-                                          named_graph_uris=parsed.get('using-named-graph-uri'))
-            elif 'update' in parsed:
-                return self.process_update(parsed['update'][0],
-                                           graph_uris=parsed.get('using-graph-uri'),
-                                           named_graph_uris=parsed.get('using-named-graph-uri'))
-        elif request.headers['content-type'] == 'application/sparql-query':
-            return self.process_query(request.data, results_format=request.headers.get('accept'),
-                                      graph_uris=request.params.get('using-graph-uri'),
-                                      named_graph_uris=request.params.get('using-named-graph-uri'))
-        elif request.headers['content-type'] == 'application/sparql-update':
-            return self.process_update(request.data,
-                                       graph_uris=request.params.get('using-graph-uri'),
-                                       named_graph_uris=request.params.get('using-named-graph-uri'))
+    def handle_post(self, request: Request, context) -> str:
+        # Want the raw string, because mocker lower-cases
+        parsed = urlparse(request._request.url)
+        qs = parse_qs(parsed.query)
+        content_type = request.headers['Content-Type']
+        if content_type == 'application/x-www-form-urlencoded':
+            parsed_body = parse_qs(request.text)
+            if 'query' in parsed_body:
+                status, headers, text = \
+                    self.process_query(parsed_body['query'][0], results_format=request.headers.get('Accept'),
+                                       graph_uris=qs.get('using-graph-uri'),
+                                       named_graph_uris=qs.get('using-named-graph-uri'))
+            elif 'update' in parsed_body:
+                status, headers, text = \
+                    self.process_update(parsed_body['update'][0],
+                                        graph_uris=qs.get('using-graph-uri'),
+                                        named_graph_uris=qs.get('using-named-graph-uri'))
+            else:
+                status, headers, text = 400, {}, "Unable to parse request body"
+        elif content_type == 'application/sparql-query':
+            status, headers, text = \
+                self.process_query(request.text, results_format=request.headers.get('Accept'),
+                                   graph_uris=qs.get('using-graph-uri'),
+                                   named_graph_uris=qs.get('using-named-graph-uri'))
+        elif content_type == 'application/sparql-update':
+            status, headers, text = \
+                self.process_update(request.text,
+                                    graph_uris=qs.get('using-graph-uri'),
+                                    named_graph_uris=qs.get('using-named-graph-uri'))
+        else:
+            status, headers, text = 415, {}, f"Unrecognized content type: {content_type}"
 
-    def handle_get(self, request: Request, context: dict) -> HTTPResponse:
-        if 'query' in request.params:
-            return self.process_query(request.params['query'], results_format=request.headers.get('accept'),
-                                      graph_uris=request.params.get('using-graph-uri'),
-                                      named_graph_uris=request.params.get('using-named-graph-uri'))
-        elif 'update' in request.params:
-            return self.process_update(request.params['update'],
-                                       graph_uris=request.params.get('using-graph-uri'),
-                                       named_graph_uris=request.params.get('using-named-graph-uri'))
+        context.status_code = status
+        if status != 200:
+            context.reason = text
+        context.headers.update(headers)
 
-        return HTTPResponse(body="Unable to parse request", status=400)
+        return text
+
+    def handle_get(self, request: Request, context) -> str:
+        # Want the raw string, because mocker lower-cases
+        parsed = urlparse(request._request.url)
+        qs = parse_qs(parsed.query)
+        if 'query' in qs:
+            status, headers, text = \
+                self.process_query(qs['query'][0], results_format=request.headers.get('Accept'),
+                                   graph_uris=qs.get('using-graph-uri'),
+                                   named_graph_uris=qs.get('using-named-graph-uri'))
+        elif 'update' in qs:
+            status, headers, text = \
+                self.process_update(qs['update'][0],
+                                    graph_uris=qs.get('using-graph-uri'),
+                                    named_graph_uris=qs.get('using-named-graph-uri'))
+        else:
+            status, headers, text = 400, {}, "Unable to parse request"
+
+        context.status_code = status
+        if status != 200:
+            context.reason = text
+        context.headers.update(headers)
+
+        return text
 
     TABLE_MEDIA_TYPES = {
         'text/plain': 'txt',
@@ -88,16 +118,18 @@ class Endpoint:
     RDF_MEDIA_TYPES = {
         'text/plain': 'txt',
         'text/csv': 'csv',
+        'text/turtle': 'turtle',
         'application/json': 'json',
         'application/ld+json': 'json',
         'application/rdf+xml': 'xml'
     }
 
-    def process_query(self, query, results_format=None, graph_uris=None, named_graph_uris=None):
+    def process_query(self, query, results_format=None,
+                      graph_uris=None, named_graph_uris=None) -> (int, dict, str):
         try:
             parsed_query = prepareQuery(query)
         except ParseException as pe:
-            return HTTPResponse(body=f"Malformed UPDATE: {pe} in {query}", status=400)
+            return 400, {}, f"Malformed query: {pe} in {query}"
         # TODO patch query.algebra.datasetClause with graph_uris (FROM) and named_graph_uris (FROM NAMED)
         # parsed_query.algebra.name will be SelectQuery, ConstructQuery or AskQuery
         if parsed_query.algebra.name == 'SelectQuery':
@@ -115,38 +147,34 @@ class Endpoint:
             mapped_format = self.TABLE_MEDIA_TYPES.get(results_format)
 
         if mapped_format is None:
-            return HTTPResponse(body=f"Unsupported result type {results_format}", status=415)
+            return 415, {}, f"Unsupported result type {results_format}"
 
         try:
             results = self.graph.query(parsed_query)
             if parsed_query.algebra.name == 'SelectQuery':
-                return HTTPResponse(body=results.serialize(format=mapped_format),
-                                    headers={'content-type': results_format},
-                                    status=200)
+                return 200, {'Content-type': results_format}, \
+                       results.serialize(format=mapped_format).decode('utf-8')
             elif parsed_query.algebra.name == 'ConstructQuery':
-                return HTTPResponse(body=results.graph.serialize(format=mapped_format),
-                                    headers={'content-type': results_format},
-                                    status=200)
+                return 200, {'Content-type': results_format}, \
+                       results.graph.serialize(format=mapped_format)
             else:
-                return HTTPResponse(body=str(results.askAnswer),
-                                    headers={'content-type': results_format},
-                                    status=200)
+                return 200, {'content-type': results_format}, str(results.askAnswer)
         except Exception as e:
-            return HTTPResponse(body=f"Error {e} occurred when evaluating {query}", status=500)
+            return 500, {}, f"Error {e} occurred when evaluating {query}"
 
-    def process_update(self, query, graph_uris=None, named_graph_uris=None):
+    def process_update(self, query, graph_uris=None, named_graph_uris=None) -> (int, dict, str):
         try:
             parsed_query = translateUpdate(parseUpdate(query))
         except ParseException as pe:
-            return HTTPResponse(body=f"Malformed UPDATE: {pe} in {query}", status=400)
+            return 400, {}, f"Malformed UPDATE: {pe} in {query}"
 
         # TODO patch query.algebra.datasetClause with graph_uris (FROM) and named_graph_uris (FROM NAMED)
 
         try:
             self.graph.update(parsed_query)
         except Exception as e:
-            return HTTPResponse(body=f"Error {e} occurred when evaluating {query}", status=500)
-        return HTTPResponse(body=f"Updated", status=200)
+            return 500, {}, f"Error {e} occurred when evaluating {query}"
+        return 200, {}, "Updated"
 
 
 @pytest.fixture
--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,6 +1,9 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+import pytest
+import requests
 
 
+@pytest.mark.skip(reason="urlopen wiring not yet done")
 def test_basic_select(sparql_endpoint):
     repo_uri = 'https://my.rdfdb.com/repo/sparql'
     rdf_files = ['tests/upper_ontology.ttl',
@@ -14,4 +17,119 @@ def test_basic_select(sparql_endpoint):
     results = sparql.query()
 
     expected = endpoint.graph.query(query).serialize(format='json')
+    assert results == expected
+
+
+def test_request_get(sparql_endpoint):
+    repo_uri = 'https://my.rdfdb.com/repo/sparql'
+    rdf_files = ['tests/upper_ontology.ttl',
+                 'tests/domain_ontology.ttl',
+                 'tests/instance_data.ttl']
+    endpoint = sparql_endpoint(repo_uri, rdf_files)
+    query = "select distinct ?class where { [] a ?class } order by ?class"
+    response = requests.get(url=repo_uri, params={'query': query}, headers={'Accept': 'application/json'})
+    results = response.text
+
+    expected = endpoint.graph.query(query).serialize(format='json').decode('utf-8')
+    assert results == expected
+
+    query = 'construct { <http://example.com/_t1> ?p ?o } where { <http://example.com/_t1> ?p ?o }'
+    response = requests.get(url=repo_uri, params={'query': query}, headers={'Accept': 'text/turtle'})
+    results = response.text
+
+    expected = endpoint.graph.query(query).serialize(format='turtle').decode('utf-8')
+    assert results == expected
+
+
+def test_request_update_get(sparql_endpoint):
+    repo_uri = 'https://my.rdfdb.com/repo/sparql'
+    rdf_files = ['tests/upper_ontology.ttl',
+                 'tests/domain_ontology.ttl',
+                 'tests/instance_data.ttl']
+    endpoint = sparql_endpoint(repo_uri, rdf_files)
+    query = "select (count(?person) as ?num) where { ?person a <http://example.com/Person> }"
+    response = requests.get(url=repo_uri, params={'query': query}, headers={'Accept': 'application/json'})
+    print(response.json())
+    assert response.json()['results']['bindings'][0]['num']['value'] == '1'
+
+    update = "insert { ?instance a ?super } " \
+             "where { ?instance a/<http://www.w3.org/2000/01/rdf-schema#subClassOf> ?super }"
+    response = requests.get(url=repo_uri, params={'update': update})
+    assert response.status_code == 200
+
+    query = "select (count(?person) as ?num) where { ?person a <http://example.com/Person> }"
+    response = requests.get(url=repo_uri, params={'query': query}, headers={'Accept': 'application/json'})
+    assert response.json()['results']['bindings'][0]['num']['value'] == '3'
+
+
+def test_request_update_post(sparql_endpoint):
+    repo_uri = 'https://my.rdfdb.com/repo/sparql'
+    rdf_files = ['tests/upper_ontology.ttl',
+                 'tests/domain_ontology.ttl',
+                 'tests/instance_data.ttl']
+    endpoint = sparql_endpoint(repo_uri, rdf_files)
+    query = "select (count(?person) as ?num) where { ?person a <http://example.com/Person> }"
+    response = requests.get(url=repo_uri, params={'query': query}, headers={'Accept': 'application/json'})
+    print(response.json())
+    assert response.json()['results']['bindings'][0]['num']['value'] == '1'
+
+    update = "insert { ?instance a ?super } " \
+             "where { ?instance a/<http://www.w3.org/2000/01/rdf-schema#subClassOf> ?super }"
+    response = requests.post(url=repo_uri, data={'update': update})
+    assert response.status_code == 200
+
+    query = "select (count(?person) as ?num) where { ?person a <http://example.com/Person> }"
+    response = requests.get(url=repo_uri, params={'query': query}, headers={'Accept': 'application/json'})
+    assert response.json()['results']['bindings'][0]['num']['value'] == '3'
+
+
+def test_request_update_post_raw(sparql_endpoint):
+    repo_uri = 'https://my.rdfdb.com/repo/sparql'
+    rdf_files = ['tests/upper_ontology.ttl',
+                 'tests/domain_ontology.ttl',
+                 'tests/instance_data.ttl']
+    endpoint = sparql_endpoint(repo_uri, rdf_files)
+    query = "select (count(?person) as ?num) where { ?person a <http://example.com/Person> }"
+    response = requests.get(url=repo_uri, params={'query': query}, headers={'Accept': 'application/json'})
+    print(response.json())
+    assert response.json()['results']['bindings'][0]['num']['value'] == '1'
+
+    update = "insert { ?instance a ?super } " \
+             "where { ?instance a/<http://www.w3.org/2000/01/rdf-schema#subClassOf> ?super }"
+    response = requests.post(url=repo_uri, data=update.encode('utf-8'),
+                             headers={'Content-Type': 'application/sparql-update'})
+    assert response.status_code == 200
+
+    query = "select (count(?person) as ?num) where { ?person a <http://example.com/Person> }"
+    response = requests.get(url=repo_uri, params={'query': query}, headers={'Accept': 'application/json'})
+    assert response.json()['results']['bindings'][0]['num']['value'] == '3'
+
+
+def test_request_post(sparql_endpoint):
+    repo_uri = 'https://my.rdfdb.com/repo/sparql'
+    rdf_files = ['tests/upper_ontology.ttl',
+                 'tests/domain_ontology.ttl',
+                 'tests/instance_data.ttl']
+    endpoint = sparql_endpoint(repo_uri, rdf_files)
+    query = "select distinct ?class where { [] a ?class } order by ?class"
+    response = requests.post(url=repo_uri, data={'query': query}, headers={'Accept': 'application/json'})
+    results = response.text
+
+    expected = endpoint.graph.query(query).serialize(format='json').decode('utf-8')
+    assert results == expected
+
+
+def test_request_post_raw(sparql_endpoint):
+    repo_uri = 'https://my.rdfdb.com/repo/sparql'
+    rdf_files = ['tests/upper_ontology.ttl',
+                 'tests/domain_ontology.ttl',
+                 'tests/instance_data.ttl']
+    endpoint = sparql_endpoint(repo_uri, rdf_files)
+    query = "select distinct ?class where { [] a ?class } order by ?class"
+    expected = endpoint.graph.query(query).serialize(format='json').decode('utf-8')
+
+    response = requests.post(url=repo_uri, data=query.encode('utf-8'),
+                             headers={'Content-Type': 'application/sparql-query',
+                                      'Accept': 'application/json'})
+    results = response.text
     assert results == expected
